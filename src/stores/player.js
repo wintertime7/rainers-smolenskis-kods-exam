cc445e6bb89856ad826284ebfb5492586ef2e1cd
wintertime7 <sgwa2001@gmail.com>
2022-11-14 16:37:30 +0200

    player.js tasks completed... hopefuly

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,42 +1,66 @@
-import { reactive } from 'vue'
+import { reactive } from "vue";
 
 export const player = reactive({
-    playlist   : [],
-    now_playing: {}, // SONG OBJECT
-    setPlaylist(songs) {
+  playlist: [],
+  now_playing: {}, // SONG OBJECT
+  setPlaylist(songs) {
+    this.playlist = songs;
+  },
+  setNowPlaying(song) {
+    this.now_playing = song;
+  },
+  getNowPlayingSongId() {
+    return this.now_playing?.id;
+  },
+  getNowPlaying() {
+    return this.now_playing;
+  },
+  getNowPlayingAlbumID() {
+    return this.now_playing?.album?.id ?? null;
+  },
+  getNowPlayingSongName() {
+    return this.now_playing?.name ?? "";
+  },
+  getNowPlayingSongImage() {
+    return this.now_playing?.album?.images[1].url ?? "";
+  },
+  getNowPlayingArtists() {
+    return this.now_playing?.artists?.map((artist) => artist.name).join(", ");
+  },
+  getNowPlayingSongPreview() {
+    return this.now_playing?.preview_url;
+  },
+  getNextSong() {
+    var index;
 
-    },
-    setNowPlaying(song) {
+    this.playlist.forEach((song, indexTemp) => {
+      if (song?.id == this.now_playing?.id) {
+        index = indexTemp;
+      }
+    });
 
-    },
-    getNowPlayingSongId() {
-        return this.now_playing?.id;
-    },
-    getNowPlaying() {
-
-    },
-    getNowPlayingAlbumID() {
-        return this.now_playing?.album?.id ?? null;
-    },
-    getNowPlayingSongName() {
-        return this.now_playing?.name ?? '';
-    },
-    getNowPlayingSongImage() {
-        return this.now_playing?.album?.images[1].url ?? '';
-    },
-    getNowPlayingArtists() {
-        return this.now_playing?.artists?.map(artist => artist.name).join(', ');
-    },
-    getNowPlayingSongPreview() {
-        return this.now_playing?.preview_url;
-    },
-    getNextSong(){
-
-    },
-    getPreviousSong() {
-
-    },
-    resetNowPlaying() {
-        this.now_playing = {};
+    if (this.playlist.length > index + 1) {
+      return false;
+    } else {
+      return this.playlist[index + 1];
     }
-})
+  },
+  getPreviousSong() {
+    var index;
+
+    this.playlist.forEach((song, indexTemp) => {
+      if (song?.id == this.now_playing?.id) {
+        index = indexTemp;
+      }
+    });
+
+    if (this.playlist.length < index - 1) {
+      return false;
+    } else {
+      return this.playlist[index - 1];
+    }
+  },
+  resetNowPlaying() {
+    this.now_playing = {};
+  },
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,7 @@
+<script>
+    
+</script>
+
 <template>
     <div id="login-view">
         <form class="login-form">
