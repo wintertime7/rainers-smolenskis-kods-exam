aed1533c0db407214817e1d987f3ca0b315acdaa
wintertime7 <sgwa2001@gmail.com>
2022-11-15 23:06:38 +0200

    about page kinda done and albums page some tasks done

---
+++ src/views/About.vue
@@ -0,0 +1,80 @@
+<script>
+import { auth } from '../stores/auth'
+export default {
+    data() {
+        return {
+            auth,
+
+            groupOne: false,
+            groupTwo: true,
+
+            newName: auth.user.name,
+            newSurname: auth.user.surname,
+            newCode: auth.user.code
+        }
+    },
+    methods: {
+        //Dienas beigās strādā, tākā viss ok :D
+        editFormBtn() {
+          this.groupOne = true;
+          this.groupTwo = false;
+        },
+
+        cancelBtn() {
+            this.groupOne = false;
+            this.groupTwo = true;
+        },
+
+        saveBtn() {
+            this.groupOne = false;
+            this.groupTwo = true;
+            this.auth.user.name = this.newName;
+            this.auth.user.surname = this.newSurname;
+            this.auth.user.code = this.newCode;
+        }
+    }
+}
+</script>
+
+<template>
+    <div id="about-view">
+        <div class="wrapper-header">
+            <h1>ABOUT ME</h1>
+            <div class="settings">
+                <div v-if="groupTwo"><button id="btn-show-favorites" @click="editFormBtn()">Edit Form</button></div>
+                <div v-if="groupOne"><button id="btn-show-favorites" @click="cancelBtn()" :class="{active: groupOne}">Cancel</button></div>
+                <div v-if="groupOne"><button id="btn-save" @click="saveBtn()">Save Form</button></div>
+            </div>
+        </div>
+        <form>
+            <div class="wrapper-input">
+                <label>NAME</label>
+                <div v-if="groupOne"><input v-model="newName" id="input-name" /></div>
+                <div v-if="groupTwo"><p id="txt-name">{{ auth.user.name }}</p></div>
+            </div>
+            <div class="wrapper-input">
+                <label>SURNAME</label>
+                <div v-if="groupOne"><input v-model="newSurname" id="input-surname" /></div>
+                <div v-if="groupTwo"><p id="txt-surname">{{ auth.user.surname }}</p></div>
+            </div>
+            <div class="wrapper-input">
+                <label>STUDENT CODE</label>
+                <div v-if="groupOne"><input v-model="newCode" id="input-code" /></div>
+                <div v-if="groupTwo"><p id="txt-code">{{ auth.user.code }}</p></div>
+            </div>
+            <div class="wrapper-songs">
+                <label>FAVORITE SONGS</label>
+                <ul>
+                    <li>
+                        <img id="img-album" src="https://i.scdn.co/image/ab67616d00001e02980c9d288a180838cd12ad24" />
+                        <div class="song-info">
+                            <p id="txt-song" class="song-name">DEEP (feat. Nonô)</p>
+                            <p id="txt-artist" class="song-artists">Example</p>
+                        </div>
+                    </li>
+                </ul>
+                <div id="txt-empty" class="empty">NO SONGS FOUND</div>
+            </div>
+        </form>
+    </div>
+</template>
--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,16 +1,54 @@
 <script>
 import IconGrid from '../components/icons/IconGrid.vue'
 import IconList from '../components/icons/IconList.vue'
+import songList from '../data/songs'
 
 export default {
   components: {IconGrid, IconList},
   methods: {
-    
+    getArtists(artists) {
+      let temp = '';
+      let len = Object.keys(artists).length;
+
+      artists.forEach((art, index) => {
+        if (index != len - 1) {
+          temp = temp + art.name + ", ";
+        } else {
+          temp = temp + art.name;
+        }
+      });
+      return temp;
+    },
+
+    getAlbumYear(date) {
+      return date.split('-')[0];
+    },
+
+    getAlbumSongCount() {
+
+    }
   },
   data() {
     return {
       isIconGrid: false,
-      isIconList: false
+      isIconList: true,
+      songs: songList,
+    }
+  },
+  computed: {
+    albums() {
+      let albums = [];
+      this.songs.forEach((song) => {
+        let obj = {
+          id: song.album,
+          songs: []
+        };
+
+        if (albums.filter(e => e.id === song.album.id).length > 0 == false) {
+          albums.push(song.album);
+        }
+      });
+      return albums;
     }
   }
 }
@@ -22,18 +60,18 @@ export default {
     <div class="wrapper-header">
         <h1>ALBUMS</h1>
         <div class="settings">
-            <button id="btn-grid" @click="isIconGrid ? isIconGrid = false : isIconGrid = true" v-bind:class="{active: isIconGrid}"><IconGrid /></button>
-            <button id="btn-list" @click="isIconList ? isIconList = false : isIconList = true" v-bind:class="{active: isIconList}"><IconList /></button>
+            <button id="btn-grid" @click="isIconGrid ? isIconGrid = false : isIconGrid = true, isIconList = false" v-bind:class="{active: isIconGrid}"><IconGrid /></button>
+            <button id="btn-list" @click="isIconList ? isIconList = false : isIconList = true, isIconGrid = false" v-bind:class="{active: isIconList}"><IconList /></button>
         </div>
     </div>
-    <ul id="list-albums">
-        <li class="album">
-            <img id="img-album" src="https://i.scdn.co/image/ab67616d00001e02980c9d288a180838cd12ad24" />
+    <ul id="list-albums" :class="{grid: isIconGrid}">
+        <li class="album" v-for="(album) in albums">
+            <img id="img-album" :src="album.images[1].url" />
             <div class="album-info">
-                <h4 id="txt-album-name">We May Grow Old But We Never Grow Up</h4>
-                <p id="txt-album-artists">Example</p>
+                <h4 id="txt-album-name">{{ album.name }}</h4>
+                <p id="txt-album-artists">{{ getArtists(album.artists) }}</p>
                 <div class="album-year">
-                    <p id="txt-album-year">2022</p>
+                    <p id="txt-album-year">{{ getAlbumYear(album.release_date) }}</p>
                     <p id="txt-album-tracks">2</p>
                 </div>
             </div>
